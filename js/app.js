f722f114760f9873bd36029fecbd2448abf5632c
Jonny Reading <jonnyreading@gmail.com>
2020-08-22 16:47:07 +0100

    can delete single or all items in list

--- js/app.js
+++ js/app.js
@@ -1,13 +1,26 @@
 document.addEventListener("DOMContentLoaded", () => {
-    console.log("loaded");
+    const populate = document.querySelector("#populate");
+    populate.addEventListener("click", handlePopulate);
+
+    const deleteAll = document.querySelector("#delete-all");
+    deleteAll.addEventListener("click", handleDeleteAll);
+
+
     const form = document.querySelector("form");
     form.addEventListener("submit", handleFormSubmit);
 });
 
+const handlePopulate = function () {
+    for (let game of games) {
+        newListItem(game.title, game.publisher, game.platform, game.rating);
+    }
+}
+
+
 const handleFormSubmit = function (event) {
     event.preventDefault();
     const title = event.target.title.value;
-    const platform = event.target.console.value;
+    const platform = event.target.platform.value;
     const publisher = event.target.publisher.value;
     const rating = event.target.rating.value;
     newListItem(title, platform, publisher, rating);
@@ -16,19 +29,22 @@ const handleFormSubmit = function (event) {
 
 const newListItem = function (title, platform, publisher, rating) {
     const gameList = document.querySelector("ul");
+    if (gameList.innerHTML === "No games in list!") {
+        gameList.innerHTML = "";
+    }
     const li = newElement("li", gameList);
     const hgroup = newElement("hgroup", li);
     newElement("h2", hgroup, title, "title");
-    newElement("h3", hgroup, platform, "console");
+    newElement("h3", hgroup, platform, "platform");
     newElement("h3", hgroup, publisher, "publisher");
     const main = newElement("main", li);
     newElement("h2", main, "User rating");
     newElement("span", main, rating, "user-rating");
-    const span = newElement("span", main, "/100 ", "rating-number");
+    const span = newElement("span", main, "/100 ");
     newInput(span, "button", "+", "increase-rating");
     newInput(span, "button", "-", "decrease-rating");
     const footer = newElement("footer", li);
-    newInput(footer, "button", "Delete", "delete-single");
+    newInput(footer, "button", "Delete", "delete-single", undefined, deleteSingleItem);
 }
 
 const newElement = function (kind, parent, content = "", classname) {
@@ -41,7 +57,7 @@ const newElement = function (kind, parent, content = "", classname) {
     return el;
 }
 
-const newInput = function (parent, type, value, classname, name) {
+const newInput = function (parent, type, value, classname, name, eventHandler) {
     const el = document.createElement("input");
     el.type = type;
     el.value = value;
@@ -49,7 +65,19 @@ const newInput = function (parent, type, value, classname, name) {
     if (classname) {
         el.classList.add(classname);
     }
+    if (eventHandler) {
+        el.addEventListener("click", eventHandler);
+    }
     parent.appendChild(el);
     return el;
 }
 
+const deleteSingleItem = function (event) {
+    const listItem = event.target.parentNode.parentNode;
+    listItem.parentNode.removeChild(listItem);
+}
+
+const handleDeleteAll = function () {
+    const gameList = document.querySelector("ul");
+    gameList.innerHTML = "No games in list!"
+}
---
+++ js/games.js
@@ -0,0 +1,20 @@
+const games = [
+    {
+        "title": "The Last of Us Part II",
+        "publisher": "Naughty Dog",
+        "platform": "PS4",
+        "rating": 100
+    },
+    {
+        "title": "Sonic the Hedgehog 2",
+        "publisher": "SEGA",
+        "platform": "Megadrive",
+        "rating": 80
+    },
+    {
+        "title": "Red Dead Redemption 2",
+        "publisher": "Rockstar",
+        "platform": ["PS4", "XBox One", "PC"],
+        "rating": 90
+    }
+];
