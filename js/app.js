247f64f1218762245e2ef83f21d2daff82b863fb
Jonny Reading <jonnyreading@gmail.com>
2020-08-22 14:41:08 +0100

    take values from form and craete new list item

--- js/app.js
+++ js/app.js
@@ -1,23 +1,34 @@
 document.addEventListener("DOMContentLoaded", () => {
     console.log("loaded");
-    newListItem();
-    newListItem();
+    // newListItem();
+    // newListItem();
+    const form = document.querySelector("form");
+    form.addEventListener("submit", newListItem);
 });
 
-const newListItem = function () {
+const newListItem = function (event) {
+    event.preventDefault();
+    console.log(event);
+    const title = event.target.title.value;
+    const platform = event.target.console.value;
+    const publisher = event.target.publisher.value;
+    const rating = event.target.rating.value;
+
     const gameList = document.querySelector("ul");
     const li = newElement("li", gameList);
     const hgroup = newElement("hgroup", li);
-    newElement("h2", hgroup, "The Last Of Us Part II", "title");
-    newElement("h3", hgroup, "PS4", "console");
-    newElement("h3", hgroup, "Naughty Dog", "publisher");
+    newElement("h2", hgroup, title, "title");
+    newElement("h3", hgroup, platform, "console");
+    newElement("h3", hgroup, publisher, "publisher");
     const main = newElement("main", li);
     newElement("h2", main, "User rating");
-    newElement("span", main, 50, "user-rating");
+    newElement("span", main, rating, "user-rating");
     const span = newElement("span", main, "/100 ");
     newInput(span, "button", "+", "increase-rating");
     const footer = newElement("footer", li);
     newInput(footer, "button", "Delete", "delete-single");
+
+    event.target.reset();
 }
 
 const newElement = function (kind, parent, content = "", classname) {
