3fb997f285303e48ebbd82d319bbaf33e0c57e89
Jonny Reading <jonnyreading@gmail.com>
2020-08-23 16:37:18 +0100

    added more to default data

--- js/games.js
+++ js/games.js
@@ -2,19 +2,43 @@ const games = [
     {
         "title": "The Last of Us Part II",
         "publisher": "Naughty Dog",
-        "platform": "PS4",
-        "rating": 100
+        "platform": ["PS4"],
+        "rating": 94
     },
     {
         "title": "Sonic the Hedgehog 2",
         "publisher": "SEGA",
-        "platform": "Megadrive",
-        "rating": 80
+        "platform": ["Megadrive"],
+        "rating": 82
     },
     {
         "title": "Red Dead Redemption 2",
         "publisher": "Rockstar",
-        "platform": ["PS4", "XBox One", "PC"],
-        "rating": 90
+        "platform": ["PS4", "Xbox One", "PC"],
+        "rating": 97
+    },
+    {
+        "title": "The Simpsons Wrestling",
+        "publisher": "Big Ape Productions",
+        "platform": ["Playstation"],
+        "rating": 32
+    },
+    {
+        "title": "Cuphead",
+        "publisher": "Studio MDHR",
+        "platform": ["PS4", "PC", "Switch", "Xbox One"],
+        "rating": 87
+    },
+    {
+        "title": "Star Wars Episode 1: Racer",
+        "publisher": "LucasArts",
+        "platform": ["PC", "Switch", "PS4"],
+        "rating": 86
+    },
+    {
+        "title": "Spongebob Squigglepants",
+        "publisher": "WayForward",
+        "platform": ["Wii", "3DS"],
+        "rating": 59
     }
 ];
